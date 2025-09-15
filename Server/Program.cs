c72d3d415c4d4d7fbe0d88104f86adaabe65fb9c
sseepun <sarun_sla@hotmail.com>
2025-09-15 16:45:11 +0700

    Update migration

--- Server/Program.cs
+++ Server/Program.cs
@@ -16,8 +16,20 @@ builder.Services.AddControllers()
     .AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNamingPolicy = null; });
 
 builder.Services.AddTransient<SystemDbContext>();
-builder.Services.AddDbContext<SystemDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<SystemDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseSeeding((context, serviceProvider) => {
+            var dbConfig = context.Set<Config>();
+            var exists = dbConfig.Where(d => d.Name == "PisTokenUrl").FirstOrDefault();
+            if (exists == null) {
+                dbConfig.Add(new Config { Name = "PisTokenUrl", Value = "https://pttapi-dev.pttplc.com/oauth2/token" });
+                dbConfig.Add(new Config { Name = "PisTokenUsername", Value = "BpJWgmtedCWbwLDmwjBcTgZT8S8a" });
+                dbConfig.Add(new Config { Name = "PisTokenPassword", Value = "f0XROG5F5FU2FMqOWE21mf89BXYa" });
+                dbConfig.Add(new Config { Name = "PisUrl", Value = "https://pttapigw-dev.pttplc.com/PTT_PIS" });
+                context.SaveChanges();
+            }
+        });
+});
 
 builder.Services.AddAuthentication(option => { option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
     .AddJwtBearer(options => { options.TokenValidationParameters = new TokenValidationParameters{ ValidateIssuer = true, ValidateAudience = true, ValidateLifetime = true, ValidateIssuerSigningKey = true }; });
